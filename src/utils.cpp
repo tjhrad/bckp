ad7ce72972c6b0e87dedbf1f2458e85d98fc0842
Trevor Huff <trevh5@gmail.com>
2024-02-05 23:15:14 -0700

    Backup working.

--- src/utils.cpp
+++ src/utils.cpp
@@ -31,7 +31,7 @@ int unidirectional_directory_backup(std::string main_directory, std::string back
         if ( std::filesystem::is_directory(entry.path()) )
         {
             std::string dir = entry.path().string();
-            std::filesystem::path temporary_stem = dir.erase(dir.find(main_directory),main_directory.length());//entry.path().string()//entry.path().stem();
+            std::filesystem::path temporary_stem = dir.erase(dir.find(main_directory),main_directory.length());
             std::filesystem::path temporary_backup_directory_path = backup_directory_path / temporary_stem.relative_path();
             if(!check_if_exists(temporary_backup_directory_path))
             {
@@ -57,6 +57,7 @@ int unidirectional_directory_backup(std::string main_directory, std::string back
                 std::uintmax_t backup_file_size = std::filesystem::file_size(temporary_backup_file_path);
                 if (main_file_size != backup_file_size)
                 {
+                    std::filesystem::remove(temporary_backup_file_path);
                     std::filesystem::copy(entry.path(),temporary_backup_file_path);
                 }
             }
@@ -67,7 +68,39 @@ int unidirectional_directory_backup(std::string main_directory, std::string back
         }
         
     }
-    //TODO: Check if backup directory has files or folders that don't exist in main directory
+
+    for (const auto & entry : std::filesystem::recursive_directory_iterator(backup_directory))
+    {
+        if ( std::filesystem::is_directory(entry.path()) )
+        {
+            std::string dir = entry.path().string();
+            std::filesystem::path temporary_stem = dir.erase(dir.find(backup_directory),backup_directory.length());
+            std::filesystem::path temporary_main_directory_path = main_directory_path / temporary_stem.relative_path();
+            if(!check_if_exists(temporary_main_directory_path))
+            {
+                std::cout << "Removing directory: " << entry.path() << std::endl;
+                std::filesystem::remove_all(entry.path());
+                
+            }
+        }
+        else if ( std::filesystem::is_regular_file(entry.path()) )
+        {
+            std::string file_path = entry.path().string();
+            std::filesystem::path temporary_filename = file_path.erase(file_path.find(backup_directory),backup_directory.length());
+            std::filesystem::path temporary_main_file_path = main_directory_path / temporary_filename.relative_path();
+
+            if(!check_if_exists(temporary_main_file_path))
+            {
+                std::cout << "Removing file: " << entry.path().string() << std::endl;
+                std::filesystem::remove(entry.path());
+            }
+        }
+        else
+        {
+            std::cout << "Unknown error with the file: " << entry.path().string() << std::endl;
+        }
+        
+    }
 
     return 1;
 }
